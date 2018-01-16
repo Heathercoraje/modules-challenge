f91094a92676d75153741ff2670009e7b77e283c
heathercoraje <yhk7830@hotmail.com>
2018-01-16 11:29:08 +0900

    add working codes add working codes but creating another repo for practicing router and handler

--- src/handler.js
+++ src/handler.js
@@ -2,79 +2,26 @@ const fs = require('fs');
 const path = require('path');
 const repos = require('./repos.json');
 const readFiles = require('./helper.js');
+const handleAPI = require('./helper.js');
+const serverError = require('./helper.js');
+
 
 const handler = (req, res) => {
   const url = req.url;
   const basePath = __dirname.replace('src', '');
 
   if (url === '/') {
-    readFiles(basePath, req, res, url);
-
-   //    fs.readFile(path.join(basePath, 'public/fac.html'), 'utf8', (err, file) => {
-   //    /* istanbul ignore if */
-   //    if (err) {
-   //      res.writeHead(500, {'content-type': 'text/plain'});
-   //      res.end('server error');
-   //    } else {
-   //      res.writeHead(200, {'content-type': 'text/html'});
-   //      res.end(file);
-   //    }
-   // });
+    readFiles(basePath.concat('public/fac.html'), 'text/html', req, res);
   } else if (url === '/fac') {
-    fs.readFile(path.join(basePath, 'public/fac.html'), 'utf8', (err, file) => {
-      /* istanbul ignore if */
-      if (err) {
-        res.writeHead(500, {'content-type': 'text/plain'});
-        res.end('server error');
-      } else {
-        res.writeHead(200, {'content-type': 'text/html'});
-        res.end(file);
-      }
-    });
+    readFiles(basePath.concat('public/fac.html'), 'text/html', req, res);
   } else if (url === '/dwyl') {
-    fs.readFile(path.join(basePath, 'public/dwyl.html'), 'utf8', (err, file) => {
-      /* istanbul ignore if */
-      if (err) {
-        res.writeHead(500, {'content-type': 'text/plain'});
-        res.end('server error');
-      } else {
-        res.writeHead(200, {'content-type': 'text/html'});
-        res.end(file);
-      }
-    });
+    readFiles(basePath.concat('public/dwyl.html'), 'text/html', req, res);
   } else if (url === '/stylesheet.css') {
-    fs.readFile(path.join(basePath, '/public/stylesheet.css'), 'utf8', (err, file) => {
-      /* istanbul ignore if */
-      if (err) {
-        res.writeHead(500, {'content-type': 'text/plain'});
-        res.end('server error');
-      } else {
-        res.writeHead(200, {'content-type': 'text/css'});
-        res.end(file);
-      }
-    });
+    readFiles(basePath.concat('public/stylesheet.css'), 'text/css', req, res);
   } else if (url === '/js/request.js') {
-    fs.readFile(path.join(basePath, 'public/js/request.js'), 'utf8', (err, file) => {
-      /* istanbul ignore if */
-      if (err) {
-        res.writeHead(500, {'content-type': 'text/plain'});
-        res.end('server error');
-      } else {
-        res.writeHead(200, {'content-type': 'text/javascript'});
-        res.end(file);
-      }
-    });
+    readFiles(basePath.concat('public/js/request.js'), 'text/javascript', req, res);
   } else if (url === '/js/index.js') {
-    fs.readFile(path.join(basePath, 'public/js/index.js'), 'utf8', (err, file) => {
-      /* istanbul ignore if */
-      if (err) {
-        res.writeHead(500, {'content-type': 'text/plain'});
-        res.end('server error');
-      } else {
-        res.writeHead(200, {'content-type': 'text/javascript'});
-        res.end(file);
-      }
-    });
+    readFiles(basePath.concat('public/js/index.js'), 'text/javascript', req, res);
   } else if (url === '/api/repos/fac') {
     res.writeHead(200, {'content-type': 'application/json'});
     res.end(JSON.stringify(repos.fac));
@@ -82,9 +29,9 @@ const handler = (req, res) => {
     res.writeHead(200, {'content-type': 'application/json'});
     res.end(JSON.stringify(repos.dwyl));
   } else {
-    res.writeHead(404, {'content-type': 'text/plain'});
-    res.end('404 server error');
-  }
+      res.writeHead(404, {'content-type': 'text/plain'});
+      res.end('404 server error');
+    }
 }
 
 
--- src/helper.js
+++ src/helper.js
@@ -1,54 +1,18 @@
 const fs = require('fs');
 const path = require('path');
 
-// module.exports = {
-//   readFiles: readFiles,
-//   handleAPI: handleAPI,
-//   serverError: serverError
-// }
-
-  // function readFiles(basepath, req, res, url) {
-  //   const extension= url.split('.')[1];
-  //   const contentType = url.split('/').length === 2 ? extension === 'css' ? 'text/css' : 'text/javascript': 'text/html';
-  //
-  //   fs.readFile(path.join(basepath, 'public/fac.html'), 'utf8', (err, file) => {
-  //     console.log(path, file);
-  //     if (err) {
-  //       res.writeHead(500, {'content-type': 'text/plain'});
-  //       res.end('Server error');
-  //     } else {
-  //       res.writeHead(200, {'content-type': `${contentType}`});
-  //       res.end(file);
-  //     }
-  //   });
-  // }
- //
-  function readFiles(basePath, req, res, url) {
-    const extension= url.split('.')[1];
-    const contentType = url.split('/').length === 2 ? 'text/javascript': 'text/html';
-    fs.readFile(path.join(basePath, 'public/fac.html'), 'utf8', (err, file) => {
-    /* istanbul ignore if */
-    if (err) {
-      res.writeHead(500, {'content-type': 'text/plain'});
-      res.end('server error');
-    } else {
-      res.writeHead(200, {'content-type': 'text/html'});
-      console.log(contentType);
-      console.log(url.split('.'));
-      res.end(file);
-    }
-  });
- }
-
-  // define handleAPI function
-  function handleAPI(json) {
-    res.writeHead(200, {'content-type': 'application/json'});
-    res.end(JSON.stringify(json));
-  }
-  // define serverError function
-  function serverError() {
-    res.writeHead(404, {'content-type': 'text/plain'});
-    res.end('404 server error');
-  }
-
 module.exports = readFiles;
+
+function readFiles(path, type, req, res) {
+
+  fs.readFile(path, 'utf8', (err, file) => {
+  /* istanbul ignore if */
+  if (err) {
+    res.writeHead(500, {'content-type': 'text/plain'});
+    res.end('server error');
+  } else {
+    res.writeHead(200, {'content-type': type});
+    res.end(file);
+  }
+  });
+}
